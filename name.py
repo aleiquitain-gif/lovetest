5505b9fd3139cbc02ebcbf35f5dd879ec67c0769
aleiquitain-gif <aleiquitain@gmail.com>
2026-09-17 10:25:43 +0800

    Create name.py

---
+++ name.py
@@ -0,0 +1,5 @@
+print('Hello, friend!')
+my_name=input("What is your name?: ")
+color=input("Nice to meet you, " + my_name + ". What is your favorite color?: ")
+print(color + " is such a beautiful color. Goodbye, " + my_name + "!")
+
